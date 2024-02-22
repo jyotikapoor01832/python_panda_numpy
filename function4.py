90008b6620108760bad6840d249f4f58513762c2
jyotikapoor01832 <jyotikapoor01832@gmail.com>
2024-02-22 21:24:57 +0530

    program to find cumulative product of numbers in a list

---
+++ function4.py
@@ -0,0 +1,10 @@
+def product(list1):
+    n=1
+    for i in list1:
+        n=n*i
+        print(n)
+ 
+list1=[1,3,5,7,9]
+print("Cumulative product of a numbers in a list is :")
+product(list1)
+print(n)
