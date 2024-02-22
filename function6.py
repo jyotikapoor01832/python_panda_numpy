9235ef7efcbe38baaee76fe5dbb814e09b5076fc
jyotikapoor01832 <jyotikapoor01832@gmail.com>
2024-02-22 22:11:05 +0530

    Mean,mode,median

---
+++ function6.py
@@ -0,0 +1,7 @@
+from statistics import mean, mode, median
+list1=[34,45,65,76,87,97,98,23,45,34]
+print("Mean is :",mean(list1))
+print("Median is :",median(list1))
+print("Mode is :",mode(list1))
+list1.sort()
+print(list1)
