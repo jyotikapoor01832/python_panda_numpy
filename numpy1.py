081e47a9b7666ec02998c6ea04b9844e97639b33
jyotikapoor01832 <jyotikapoor01832@gmail.com>
2024-02-25 21:34:09 +0530

    Creating and accessing element in 1D array

---
+++ numpy1.py
@@ -0,0 +1,6 @@
+import numpy as np
+arr=np.array([10,20,30,40])
+print(arr)
+print("First element in array is : ",arr[0])
+print("Second element in array is : ",arr[1])
+print("Last element in array is : ",arr[-1])
