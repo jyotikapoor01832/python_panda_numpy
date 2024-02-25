ec5484639d958250f18380748bb4d4c804b3f042
jyotikapoor01832 <jyotikapoor01832@gmail.com>
2024-02-25 21:52:37 +0530

    Creating and accessing 2D array using numpy

---
+++ numpy2D.py
@@ -0,0 +1,12 @@
+import numpy as np
+arr=np.array([[1,2,3],[6,7,8]])
+print(arr)
+print("Elements of array are : ", arr[0][0])
+print(arr[0][1])
+print(arr[0][2])
+
+print(arr[1][0])
+print(arr[1][1])
+print(arr[1][-1])
+
+print(  arr.ndim," dimensional array")
