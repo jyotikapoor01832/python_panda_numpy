52eecf242d3998efcef86b252eeb6967ce980f5e
jyotikapoor01832 <jyotikapoor01832@gmail.com>
2024-02-26 09:54:37 +0530

    Generate a random number and array

---
+++ numpy4.py
@@ -0,0 +1,9 @@
+#Program to generate a random number
+from numpy import random
+x=random.randint(50)
+print("Random number is :",x)
+
+#Program to generate a random array
+from numpy import random
+arr=random.randint(100,size=5)
+print("Random array is :",arr)
